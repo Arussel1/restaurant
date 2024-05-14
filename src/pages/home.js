ba2e8ee0327687f9dc4476820e469d6dc34da96c
Arussel <anhminh05102004@gmail.com>
2024-05-14 01:36:27 -0400

    Create HTML homepage layout

---
+++ src/pages/home.js
@@ -0,0 +1,7 @@
+export default function Home(contentDiv){
+    contentDiv.innerHTML = "";
+    const homeHeader = document.createElement('p');
+    const br = document.createElement('br');
+    homeHeader.classList.add = "homeHeader";
+    
+}
