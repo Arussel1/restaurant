b68ce14ff627fedea1fee848a8b5b0e379703fe2
Arussel <anhminh05102004@gmail.com>
2024-05-14 15:18:55 -0400

    Fix About page img

--- main.js
+++ main.js
@@ -136,7 +136,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
   \****************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ About)\n/* harmony export */ });\nfunction About(divContent){\n    divContent.innerHTML = \"\";\n\n    const about = document.createElement('div');\n    const img = document.createElement('img');\n    const aboutDescription = document.createElement('p');\n    img.src = \"/src/images/restaurant.jpg\";\n    img.alt = \"restaurant\";\n    aboutDescription.classList.add(\"aboutDescription\");\n    aboutDescription.textContent = \"Welcome to Airport Pho, a culinary landmark with a legacy spanning over 50 years. Since our humble beginnings, we have been serving authentic Vietnamese cuisine, delighting generations of diners with our flavorful dishes and warm hospitality. Our journey began in 1960, when \\\"Fake Name\\\" opened the doors to our first restaurant. With a passion for traditional Vietnamese flavors and a commitment to quality, we quickly became a beloved establishment in the community. Today, Airport Pho continues to uphold the same values that have defined us for decades. We source the finest ingredients, carefully preparing each dish to honor our heritage and satisfy the modern palate. Our menu features a wide array of culinary delights, from classic pho and savory spring rolls to spicy vermicelli and fragrant broken rice dishes As we celebrate over 50 years of culinary excellence, we invite you to join us on a gastronomic journey through Vietnam. Experience the taste of tradition, served with a side of history, only at Airport Pho.\";\n    about.classList.add(\"about\");\n    about.appendChild(img);\n    about.appendChild(aboutDescription);\n\n    divContent.appendChild(about);\n}\n\n//# sourceURL=webpack://restaurant/./src/pages/about.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ About)\n/* harmony export */ });\n/* harmony import */ var _src_images_restaurant_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../../src/images/restaurant.jpg */ \"./src/images/restaurant.jpg\");\n\nfunction About(divContent){\n    divContent.innerHTML = \"\";\n\n    const about = document.createElement('div');\n    const img = document.createElement('img');\n    const aboutDescription = document.createElement('p');\n    img.src = _src_images_restaurant_jpg__WEBPACK_IMPORTED_MODULE_0__;\n    img.alt = \"restaurant\";\n    aboutDescription.classList.add(\"aboutDescription\");\n    aboutDescription.textContent = \"Welcome to Airport Pho, a culinary landmark with a legacy spanning over 50 years. Since our humble beginnings, we have been serving authentic Vietnamese cuisine, delighting generations of diners with our flavorful dishes and warm hospitality. Our journey began in 1960, when \\\"Fake Name\\\" opened the doors to our first restaurant. With a passion for traditional Vietnamese flavors and a commitment to quality, we quickly became a beloved establishment in the community. Today, Airport Pho continues to uphold the same values that have defined us for decades. We source the finest ingredients, carefully preparing each dish to honor our heritage and satisfy the modern palate. Our menu features a wide array of culinary delights, from classic pho and savory spring rolls to spicy vermicelli and fragrant broken rice dishes As we celebrate over 50 years of culinary excellence, we invite you to join us on a gastronomic journey through Vietnam. Experience the taste of tradition, served with a side of history, only at Airport Pho.\";\n    about.classList.add(\"about\");\n    about.appendChild(img);\n    about.appendChild(aboutDescription);\n\n    divContent.appendChild(about);\n}\n\n//# sourceURL=webpack://restaurant/./src/pages/about.js?");
 
 /***/ }),
 
@@ -207,6 +207,16 @@ eval("module.exports = __webpack_require__.p + \"9d7a6e03c6e3ed49c7ac.ttf\";\n\n
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 eval("module.exports = __webpack_require__.p + \"6d564545f7736c6d082c.jpg\";\n\n//# sourceURL=webpack://restaurant/./src/images/background.jpg?");
+
+/***/ }),
+
+/***/ "./src/images/restaurant.jpg":
+/*!***********************************!*\
+  !*** ./src/images/restaurant.jpg ***!
+  \***********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"44607afdab590d7d2eea.jpg\";\n\n//# sourceURL=webpack://restaurant/./src/images/restaurant.jpg?");
 
 /***/ })
 
